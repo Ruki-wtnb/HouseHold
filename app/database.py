eb97ed9ded2d3c8213b569527bff81633d5bf52d
Ruki-wtnb <shinpeww@gmail.com>
2022-05-14 20:33:45 +0900

    接続先変更

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
-#https://tkstock.site/2021/12/03/heroku-sqlalchmy-create_engine-%E3%82%A8%E3%83%A9%E3%83%BC-nosuchmoduleerror/
+
+import os
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db_hh:3306/householddb?charset=utf8"
+ASYNC_DB_URL = "mysql+pymysql://root@db_hh:3306/householddb?charset=utf8".format(**{
+    'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
+    'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+    'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+    'db': os.getenv('DB_NAME', os.environ['DB_NAME']),
+})
+#"mysql+aiomysql://root@db_hh:3306/householddb?charset=utf8"
 
 async_engine  = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
--- app/migrate_db.py
+++ app/migrate_db.py
@@ -4,11 +4,22 @@ DBマイグレーション用ファイル
 # 初期化 python -c "import app.migrate_db; app.migrate_db.reset_database()"
 #　作成　　python -m app.migrate_db
 
-from sqlalchemy import create_engine
+# >>> from app.migrate_db import create_database
+# >>> create_database()
 
+import os
+from sqlalchemy import create_engine
 from .models import Base
 
-DB_URL = "mysql+pymysql://root@db_hh:3306/householddb?charset=utf8"
+DB_URL = "mysql+pymysql://root@db_hh:3306/householddb?charset=utf8".format(**{
+    'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
+    'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+    'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+    'db': os.getenv('DB_NAME', os.environ['DB_NAME']),
+})
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
+#"mysql+pymysql://root@db_hh:3306/householddb?charset=utf8"
 
 engine = create_engine(DB_URL, echo=True)
 
