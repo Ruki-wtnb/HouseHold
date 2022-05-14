9984cc1db119b52d46962c75e885a2a89e39fcd9
Ruki-wtnb <shinpeww@gmail.com>
2022-05-14 17:45:50 +0900

    db修正

--- app/database.py
+++ app/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db_hh:3306/householddb?charset=utf8"
+ASYNC_DB_URL = 'mysql://bf66734e389b69:559f48c7@us-cdbr-east-05.cleardb.net/heroku_ab32341ec7387b1?reconnect=true'
+#"mysql+aiomysql://root@db_hh:3306/householddb?charset=utf8"
 
 async_engine  = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
