3bc95c2050cce7fc91e8a7a2d74d0532ec667da2
Ruki-wtnb <shinpeww@gmail.com>
2022-05-16 08:11:49 +0900

    修正

--- app/models.py
+++ app/models.py
@@ -24,14 +24,14 @@ from sqlalchemy.ext.declarative import AbstractConcreteBase
 
 class FixedCostCategory(Base):
     __tablename__ = 'fixed_cost_categories'
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = {'extend_existing': True, "mysql_charset": "utf8mb4"}
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     en_name = Column(String(50), nullable=False)
 
 class VariableCostCategory(Base):
     __tablename__ = 'variable_cost_categories'
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = {'extend_existing': True, "mysql_charset": "utf8mb4"}
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     en_name = Column(String(50), nullable=False)
@@ -39,26 +39,27 @@ class VariableCostCategory(Base):
 
 class FixedCost(Base):
     __tablename__ = 'fixed_costs'
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = {'extend_existing': True, "mysql_charset": "utf8mb4"}
     year_month = Column(String(10), primary_key=True)
     price = Column(Integer, nullable=False)
-    fixed_category_id = Column(Integer, ForeignKey('fixed_cost_categories.id'), primary_key=True)
-    fixed_cost_category = relationship("FixedCostCategory", back_populates="fixedCosts", lazy='selectin', innerjoin=True)
+    # fixed_category_id = Column(Integer, ForeignKey('fixed_cost_categories.id'), primary_key=True)
+    # fixed_cost_category = relationship("FixedCostCategory", back_populates="fixedCosts", lazy='selectin', innerjoin=True)
 
 
 class VariableCost(Base):
     __tablename__ = 'variable_costs'
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = {'extend_existing': True, "mysql_charset": "utf8mb4"}
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     date = Column(Date, nullable=False)
     name = Column(String(100), nullable=False)
     price = Column(Integer, nullable=False)
     spending_flag = Column(Boolean, nullable=False)
-    variable_category_id = Column(Integer, ForeignKey('variable_cost_categories.id'), nullable=False)
-    variable_category = relationship("VariableCostCategory", back_populates="variableCosts", lazy='selectin', innerjoin=True)
+    # variable_category_id = Column(Integer, ForeignKey('variable_cost_categories.id'), nullable=False)
+    # variable_category = relationship("VariableCostCategory", back_populates="variableCosts", lazy='selectin', innerjoin=True)
 
 
 class Totals(Base):
     __tablename__ = 'totals'
+    __table_args__ = {"mysql_charset": "utf8mb4"}
     year_month = Column(String(10), primary_key=True)
     price = Column(Integer, nullable=False)
